3ca94df87da0918365a4c2ad2d056941f98d86b4
Michael Wong <michaelchunkitwong@gmail.com>
2017-02-09 22:10:15 -0500

    Redo index to spec

--- index.php
+++ index.php
@@ -1,166 +1,91 @@
 <!DOCTYPE HTML>
-
 <html>
-	<head>
-		<title>Lux Trim Mouldings and Interior Design | GTA</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta name="google-site-verification" content="0Qt12fv8k59JZWKmGhvPPrMcaxtZ-KPKc4D6gN0Huws" />
-		<meta name="description" content="Lux Trim is an interior design company, specializing in mouldings and decor. We service GTA locations: Brampton, Vaughan, Mississauga, Toronto, etc." />
 
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-	</head>
-	<body class="index">
-		<div id="page-wrapper">
+<head>
+    <title>Lux Trim Mouldings and Interior Design | GTA</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="google-site-verification" content="0Qt12fv8k59JZWKmGhvPPrMcaxtZ-KPKc4D6gN0Huws" />
+    <meta name="description" content="Lux Trim is an interior design company, specializing in mouldings and decor. We service GTA locations: Brampton, Vaughan, Mississauga, Toronto, etc." />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="assets/css/ie8.css" />
+        <![endif]-->
+    <!--[if lte IE 9]>
+        <link rel="stylesheet" href="assets/css/ie9.css" />
+        <![endif]-->
+</head>
 
-			<!-- Header -->
-			<?php include 'shared/header.php';?>
+<body class="index">
+    <div id="page-wrapper">
+        <!-- Header -->
+        <?php include 'shared/header.php';?>
+        <!-- 1nner -->
+        <section id="banner">
+            <div id="home-banner wrapper" style="max-width:100%;">
+                <img style="width:100%;" src="assets/images/homebanner.jpg">
+            </div>
+        </section>
+        <article id="main">
+            <header class="special container wrapper">
+                <h1>BEAUTIFY YOUR HOME!</h1>
+                <h3><strong>Lux Trim</strong> is committed to furnishing your home with custom <br>
+                    mouldings and decor to perfectly match your style. We pride<br>
+                     ourselves in offering top quality materials, tailored design, <br>
+                     and the cleanest and most efficient service you'll find.
+                    </h3>
+                <br>
+                <footer class="filled-box" style="max-width:50px; margin:0 auto; display: table">
+                    <a href="about.php" class="button fit scrolly">Our Mission</a>
+                </footer>
+            </header>
+            <!-- One -->
+            <div style="padding:1.5em 0em 0.1em; background-color:#f5efdf">
+                <section class="wrapper style2 container special-alt" style="background-color:black;margin-bottom:5em;">
+                    <div style="text-align:center">
+                        <h2>BEAUTIFUL DESIGNS</h2>
+                        <footer style="max-width:50px; margin:0 auto; display: table">
+                            <ul class="buttons">
+                                <li><a href="gallery.php" class="button fit scrolly">View Gallery</a>
+                                </li>
+                            </ul>
+                        </footer>
+                    </div>
+                </section>
+            </div>
+            <header class="special container wrapper">
+                <div style="display:inline-block; width:49%; text-align:left">
+                    <h3>NEWSLETTER SIGNUP</h3>
+                    <input type="text" name="FirstName" value="Email Address">
+                    <br>
+                    <footer class="filled-box" style="max-width:30px; margin:0 auto; display: table; float:left">
+                        <a href="about.php" class="button fit scrolly">SUBSCRIBE</a>
+                    </footer>
 
-			<!-- 1nner -->
+                </div>
+                <div style="display:inline-block; width:50%">
+                    <h3>CONTACT</h3>
+                    <p>We service GTA locations
+                        <br> including Toronto, Brampton,
+                        <br> Vaughan, Mississauga etc</p>
+                    <footer class="filled-box" style="max-width:30px; margin:0 auto; display: table;">
+                        <a href="contact.php" class="button fit scrolly">GET IN TOUCH</a>
+                    </footer>
+                </div>
+            </header>
+        </article>
+    </div>
+    <?php include 'shared/footer.php';?>
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.dropotron.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/jquery.scrollgress.min.js"></script>
+    <script src="assets/js/skel.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+    <script src="assets/js/main.js"></script>
+</body>
 
-			<section id="banner">
-				<div id="home-banner wrapper" style="padding-bottom: 80px; max-width:100%;">
-					<img style="width:100%;" src="assets/images/homebanner.jpg" >
-				</div>
-				<!--
-					".inner" is set up as an inline-block so it automatically expands
-					in both directions to fit whatever's inside it. This means it won't
-					automatically wrap lines, so be sure to use line breaks where
-					appropriate (<br />).
-				-->
-				<div class="inner">
-
-					<header id="switcher">
-						<img id="lux-logo" src="assets/images/logo/logo.png">
-					</header>
-					<h3>Lux Trim is committed to furnishing your home with custom mouldings <br>
-					 and decor to perfectly match your style. We pride ourselves in offering <br>
-					  top quality materials, tailored design, and the cleanest and most efficient service you’ll find. </h3>
-					
-					<footer style="max-width:50px; margin:0 auto; display: table">
-						<a href="about.php" class="button fit scrolly">Our Mission</a>
-					</footer>
-
-				</div>
-
-			</section>
-
-			<!-- Main -->
-				<article id="main">
-
-					<header class="special container">
-						<h2><strong>Lux Trim</strong> is a company dedicated to providing the highest quality polyurethane mouldings and installation.
-						</h2>
-						<br>
-						<h2>
-							With a wide range of ceiling mouldings, crown mouldings, and other related decorations, we have established many beautiful assortments and european styles to fit to your liking.
-						</h2>
-					</header>
-
-					<!-- One -->
-						<section class="wrapper style2 container special-alt">
-								<div>
-									<h1>BEAUTIFUL DESIGNS</h1>
-									<footer>
-										<ul class="buttons">
-											<li><a href="gallery.php" class="button">View Gallery</a></li>
-										</ul>
-									</footer>
-								</div>
-						</section>			
-
-					<!-- Two -->
-						<section class="wrapper style1 container special">
-													<header class="major">
-								<h2>Why <strong><font color="333655">LUXTRIM</font></strong></h2>
-							</header>
-							<div class="row">
-								<div class="4u 12u(narrower)">
-
-									<section class="center">
-										<span class="icon featured fa-check"></span>
-
-										<p>Lasts longer than wood or MDF moulds.</p>
-									</section>
-
-								</div>
-								<div class="4u 12u(narrower)">
-
-									<section class="center">
-										<span class="icon featured fa-check"></span>
-
-										<p>Resistant to moisture, insects, and vermin.</p>
-									</section>
-
-								</div>
-								<div class="4u 12u(narrower)">
-
-									<section class="center">
-										<span class="icon featured fa-check"></span>
-
-										<p>No excessive expansion or contraction due to heat or cold; no warping; no splitting.</p>
-									</section>
-
-								</div>
-							</div>
-							<div class="row">
-								<div class="4u 12u(narrower)">
-
-									<section class="center">
-										<span class="icon featured fa-check"></span>
-
-										<p>Lightweight yet durable.</p>
-									</section>
-
-								</div>
-								<div class="4u 12u(narrower)">
-									<section class="center">
-										<span class="icon featured fa-check"></span>
-										<p>Strong resilience to bending and curvature.</p>
-									</section>
-								</div>
-								<div class="4u 12u(narrower)">
-									<section class="center">
-										<span class="icon featured fa-check"></span>
-
-										<p>Easy to cut and easy to install.</p>
-									</section>
-
-								</div>								
-							</div>
-						</section>
-					
-
-				</article>
-
-			<!-- CTA -->	
-			<section id="cta">
-				<h1>CONTACT</h1>
-					<h3>We service GTA locations including Toronto, Brampton, Vaughan, Mississauga etc. Contact us for free design consultation.</h3><br>
-					<footer>
-						<ul class="buttons">
-							<li><a href="contact.php" class="button">Get In Touch</a></li>
-						</ul>
-					</footer>
-
-				</section>
-
-		</div>
-
-		<?php include 'shared/footer.php';?>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.scrollgress.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
-
-	</body>
 </html>
