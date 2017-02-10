efb49f7187746c8714fa9495eced1ad27e74e48d
Michael Wong <michaelchunkitwong@gmail.com>
2017-02-09 22:22:18 -0500

    Make footer match specs

--- shared/footer.php
+++ shared/footer.php
@@ -4,9 +4,19 @@
 		<li><a href="https://www.facebook.com/LuxTrim/" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
 		<li><a href="https://plus.google.com/b/102898757783711865615/102898757783711865615" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>
 	</ul>
+	<img src="assets/images/improve.png">
 
-	<ul class="copyright">
-		<li>&copy; 2015 Lux Trim. All Rights Reserved</li>
-	</ul>
 
+	<div class="copyright">
+		<div style="text-align:left; width:45%; display:inline-block">
+			&copy; 2015 Lux Trim. All Rights Reserved
+		</div>
+		<div style="text-align: right; width:45%; display:inline-block">
+			<ul style="display:inline">
+				<li><a href="#">Privacy Policy</a></li>
+				<li><a href="#">Terms of Use</a></li>
+				<li><a href="#">Site Map</a></li>
+			</ul>
+		</div>
+	</div>
 </footer>
