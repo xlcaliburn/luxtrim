6499270be5c4f0a519636758587bb7f80e5b3149
Michael Wong <michaelchunkitwong@gmail.com>
2017-02-13 01:22:30 -0500

    OK I NEED TO USE BOOTSTRAP

--- about.php
+++ about.php
@@ -28,20 +28,48 @@
                     <h3>Our Mission is to revolutionize moulding design and <br>installation by fixing traditional pain points experienced by clients</h3>
                     <br><br>
                     <section style="text-align:left" class="container 75%">
-                    <h3>1. Use the best, most customizable materials.</h3>
-                    <p>After researching different materials around the world, we found one that we can call “perfect”.
-                        Learn more about our <a href="polyurethane-moulding-material.php">High Density Polyurethane material</a>
-                    </p>
-                    <h3>2. Provide the most efficient service.</h3>
-                    <p>
-                        People are tired of contractors spending months of time on renovations – And they should be. We aim to finish most projects within two weeks, from design to installation.
-                    </p>
-                    <h3>3. Free design consultation.<br>
-                    </h3>
-                    <p>  We want our clients to be 100% confident before we even start. We’ll come to your house and outline detailed, personalized changes; worth thousands of dollars - free of charge. (Applies to nearby clients in the GTA).
-                        <a href="contact.php">Book your free consultation</a>
+                        <div width=15% style="display:inline-block">
+                            <img src="assets/images/1.png">
+                        </div>
+                        <div width=80% style="display:inline-block">
+                            <h3>Use the best, most customizable materials.</h3>
+                            <p>After researching different materials around the world, we found one that we can call “perfect”.
+                                Learn more about our <a href="polyurethane-moulding-material.php">High Density Polyurethane material</a>
+                            </p>
+                        </div>
+                        <table>
+                            <tr>
+                                <td width=15%>
+                                    <img src="assets/images/1.png">
+                                </td>
+                                <td>
+                                    <h3>Use the best, most customizable materials.</h3>
+                                    <p>After researching different materials around the world, we found one that we can call “perfect”.
+                                        Learn more about our <a href="polyurethane-moulding-material.php">High Density Polyurethane material</a>
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td width=15%><img src="assets/images/2.png" width="80px"></td>
+                                <td>
+                                    <h3>Provide the most efficient service.</h3>
+                                    <p>
+                                        People are tired of contractors spending months of time on renovations – And they should be. We aim to finish most projects within two weeks, from design to installation.
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td width=15%><img src="assets/images/3.png" width="80px"></td>
+                                <td>
+                                    <h3>Free design consultation.</h3>
+                                    <p>
+                                        We want our clients to be 100% confident before we even start. We’ll come to your house and outline detailed, personalized changes; worth thousands of dollars - free of charge. (Applies to nearby clients in the GTA).
+                                        <a href="contact.php">Book your free consultation</a>
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
                 </div>
-            </p>
                 </section>
             </article>
             <?php include 'shared/footer.php';?>
